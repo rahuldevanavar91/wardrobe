df3b9c792b7c6866c37ca632d58886c33c8e72b5
purplle <rahul.d@purplle.com>
2016-11-22 12:41:27 +0530

    First commit

---
+++ Application/src/main/java/com/corwdfire/wardrode/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.corwdfire.wardrode.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.corwdfire.wardrode.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
